f40e0d93f3ba453b4feab23548b4381dea1852cc
rudrodip <official.rudrodipsarker@gmail.com>
2024-07-10 16:13:59 +0600

    updated

--- src/app.ts
+++ src/app.ts
@@ -7,13 +7,12 @@ import {
   useScene,
   useTick,
 } from "./render/init.js";
-import { SavePass } from "three/examples/jsm/postprocessing/SavePass.js";
-import { ShaderPass } from "three/examples/jsm/postprocessing/ShaderPass.js";
-import { BlendShader } from "three/examples/jsm/shaders/BlendShader.js";
-import { CopyShader } from "three/examples/jsm/shaders/CopyShader.js";
 
-import vertexShader from "./shaders/vertex.glsl?raw";
-import fragmentShader from "./shaders/fragment.glsl?raw";
+import vertexPars from "./shaders/vertex_pars.glsl?raw";
+import vertexMain from "./shaders/vertex_main.glsl?raw";
+import fragmentPars from "./shaders/fragment_pars.glsl?raw";
+import fragmentMain from "./shaders/fragment_main.glsl?raw";
+import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
 
 const startApp = () => {
   const scene = useScene();
@@ -21,35 +20,48 @@ const startApp = () => {
   const gui = useGui();
   const { width, height } = useRenderSize();
 
-  // settings
-  const MOTION_BLUR_AMOUNT = 0;
-
   // lighting
-  const dirLight = new THREE.DirectionalLight("#ffffff", 0.75);
-  dirLight.position.set(5, 5, 5);
+  const dirLight = new THREE.DirectionalLight("#526cff", 2);
+  dirLight.position.set(2, 2, 2);
 
-  const ambientLight = new THREE.AmbientLight("#ffffff", 0.2);
+  const ambientLight = new THREE.AmbientLight("#4255ff", 0.6);
   scene.add(dirLight, ambientLight);
 
-  // uniforms
-  const uniforms = {
-    u_time: { type: "f", value: 0.0 },
-    u_resolution: {
-      type: "v2",
-      value: new THREE.Vector2(
-        window.innerWidth,
-        window.innerHeight
-      ).multiplyScalar(window.devicePixelRatio),
-    },
-    u_mouse: { type: "v2", value: new THREE.Vector2(0.0, 0.0) },
-  };
-
   // meshes
-  const geometry = new THREE.IcosahedronGeometry(1, 10);
-  const material = new THREE.ShaderMaterial({
-    vertexShader,
-    fragmentShader,
-    uniforms,
+  const geometry = new THREE.IcosahedronGeometry(1, 400);
+  const material = new THREE.MeshStandardMaterial({
+    roughness: 0,
+    // @ts-ignore
+    onBeforeCompile: (shader) => {
+      // storing a reference to the shader object
+      material.userData.shader = shader;
+
+      // uniforms
+      shader.uniforms.uTime = { value: 0 };
+
+      const parsVertexString = /* glsl */ `#include <displacementmap_pars_vertex>`;
+      shader.vertexShader = shader.vertexShader.replace(
+        parsVertexString,
+        parsVertexString + vertexPars
+      );
+
+      const mainVertexString = /* glsl */ `#include <displacementmap_vertex>`;
+      shader.vertexShader = shader.vertexShader.replace(
+        mainVertexString,
+        mainVertexString + vertexMain
+      );
+
+      const mainFragmentString = /* glsl */ `#include <normal_fragment_maps>`;
+      const parsFragmentString = /* glsl */ `#include <bumpmap_pars_fragment>`;
+      shader.fragmentShader = shader.fragmentShader.replace(
+        parsFragmentString,
+        parsFragmentString + fragmentPars
+      );
+      shader.fragmentShader = shader.fragmentShader.replace(
+        mainFragmentString,
+        mainFragmentString + fragmentMain
+      );
+    },
   });
 
   const ico = new THREE.Mesh(geometry, material);
@@ -64,63 +76,50 @@ const startApp = () => {
   lightFolder.add(dirLight.position, "x", -10, 10);
   lightFolder.add(dirLight.position, "y", -10, 10);
   lightFolder.add(dirLight.position, "z", -10, 10);
+
+  lightFolder.add(dirLight, "intensity", 0, 2).name("Directional Intensity");
+  lightFolder.add(ambientLight, "intensity", 0, 2).name("Ambient Intensity");
+  lightFolder.addColor(dirLight, "color").name("Directional Color");
+  lightFolder.addColor(ambientLight, "color").name("Ambient Color");
+
   lightFolder.close();
 
   const materialFolder = gui.addFolder("Material");
   materialFolder.add(material, "wireframe");
   materialFolder.open();
 
-  const motionBlurFolder = gui.addFolder("Motion Blur");
-  motionBlurFolder
-    .add({ amount: MOTION_BLUR_AMOUNT }, "amount", 0, 1)
-    .onChange((value) => {
-      blendPass.uniforms["mixRatio"].value = value;
-    });
-  motionBlurFolder.close();
+  materialFolder.add(material, "roughness", 0, 1);
+  materialFolder.add(material, "metalness", 0, 1);
 
-  // postprocessing
-  const renderTargetParameters = {
-    minFilter: THREE.LinearFilter,
-    magFilter: THREE.LinearFilter,
-    stencilBuffer: false,
+  // Add a new folder for bloom settings
+  const bloomFolder = gui.addFolder("Bloom");
+  const bloomParams = {
+    strength: 1.24,
+    radius: 0,
+    threshold: 0.1,
   };
 
-  // save pass
-  const savePass = new SavePass(
-    new THREE.WebGLRenderTarget(width, height, renderTargetParameters)
+  bloomFolder.add(bloomParams, "strength", 0, 3).onChange(updateBloom);
+  bloomFolder.add(bloomParams, "radius", 0, 1).onChange(updateBloom);
+  bloomFolder.add(bloomParams, "threshold", 0, 1).onChange(updateBloom);
+
+  function updateBloom() {
+    bloomPass.strength = bloomParams.strength;
+    bloomPass.radius = bloomParams.radius;
+    bloomPass.threshold = bloomParams.threshold;
+  }
+
+  const bloomPass = new UnrealBloomPass(
+    new THREE.Vector2(width, height),
+    1.24,
+    0,
+    0.1
   );
+  addPass(bloomPass);
 
-  // blend pass
-  const blendPass = new ShaderPass(BlendShader, "tDiffuse1");
-  blendPass.uniforms["tDiffuse2"].value = savePass.renderTarget.texture;
-  blendPass.uniforms["mixRatio"].value = MOTION_BLUR_AMOUNT;
-
-  // output pass
-  const outputPass = new ShaderPass(CopyShader);
-  outputPass.renderToScreen = true;
-
-  // adding passes to composer
-  addPass(blendPass);
-  addPass(savePass);
-  addPass(outputPass);
-
-  useTick(({ timestamp }) => {
-    uniforms.u_time.value = timestamp / 1000;
-    ico.rotation.x += 0.01;
-    ico.rotation.y -= 0.01;
-  });
-
-  window.addEventListener("resize", () => {
-    uniforms.u_resolution.value
-      .set(window.innerWidth, window.innerHeight)
-      .multiplyScalar(window.devicePixelRatio);
-  });
-
-  window.addEventListener("mousemove", (e) => {
-    uniforms.u_mouse.value.set(
-      e.screenX / window.innerWidth,
-      1 - e.screenY / window.innerHeight
-    );
+  useTick(({ timestamp, timeDiff }) => {
+    const time = timestamp / 5000;
+    material.userData.shader.uniforms.uTime.value = time;
   });
 };
 
